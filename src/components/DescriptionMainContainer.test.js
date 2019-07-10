034a26cdc8a62f207dee8f1f60ead8fae0e7cc05
Chad Archila <chadparchila@gmail.com>
2019-07-10 11:52:01 -0700

    Remove CRUFT and add notes

--- src/components/DescriptionMainContainer.test.js
+++ src/components/DescriptionMainContainer.test.js
@@ -9,6 +9,7 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
+
 //Works, but i'm not sure why
 // describe("search component", () => {
 //   test("renders", () => {
@@ -17,20 +18,25 @@ configure({ adapter: new Adapter() });
 //   })
 // });
 
+
+//v tried with shallow, but i get an error saying 'cannot read property '0' of undefined'
 describe("search component", () => {
   test("renders", () => {
-    const wrapper = shallow(<DescriptionMainContainer />);
+    const wrapper = shallow(<DescriptionMainContainer ReviewData={[]} />);
     expect (wrapper.exists()).toBe(true);
   })
 });
 
 
+
+// v this doesn't work. Compare it to an example above to see the difference
 // describe('<DescriptionMainContainer />', () => {
 //   it('renders three <Foo /> components', () => {
 //     const wrapper = shallow(<DescriptionMainContainer />);
 //     expect(wrapper.find(DescriptionMainContainer)).to.have.lengthOf(3);
 //   });
 // }); 
+
 
 //this test work v //
 // describe('Examining the syntax of Jest tests', () => {
