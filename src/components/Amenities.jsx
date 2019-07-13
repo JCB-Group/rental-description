bca4388a39f6dee1fb85e124f8dd0b07251ac0b8
Chad Archila <chadparchila@gmail.com>
2019-07-13 09:35:42 -0700

    Fix linter errors

--- src/components/Amenities.jsx
+++ src/components/Amenities.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
-import style from './Amenities.css'
+import style from './Amenities.css';
 
 const Amenities = (props) => {
-
-  let toShow = "loading........."
+  let toShow = 'loading.........';
   let amenitiesToDisplay = null;
 
-  if(Object.keys(props.RentalData).length > 1){
-
-    if(props.RentalData.showMoreAmenities){
+  if (Object.keys(props.RentalData).length > 1) {
+    if (props.RentalData.showMoreAmenities) {
       let obj = props.RentalData.rentalData.quickDetail.amenities
-      
-      for(var key in obj){
+
+      for (var key in obj) {
         if(obj[key].length > 0){
           amenitiesToDisplay = obj[key].map(amenitie => {
             // <div>{amenitie}</div> 
@@ -21,26 +19,26 @@ const Amenities = (props) => {
       }
     }
 
-    console.log(amenitiesToDisplay)// this is null.......weird. explore this tomrrow morning
+    console.log(amenitiesToDisplay); // this is null.......weird. explore this tomrrow morning
 
 
-
-    toShow =
-    <div className={style.mainContainer}>
-      <div className={style.Amenities}>Amenities </div>
-      <div onClick={props.showMoreAmenitiesHandler} className={style.readMore}>Show all 30 amenities </div>
-      <div>{amenitiesToDisplay}</div>
-      {/* <amenitiesToDisplay/> */}
-      <hr/>
-    </div>
+    toShow =(
+      <div className={style.mainContainer}>
+        <div className={style.Amenities}>Amenities </div>
+        <div onClick={props.showMoreAmenitiesHandler} className={style.readMore}>Show all 30 amenities </div>
+        <div>{amenitiesToDisplay}</div>
+        {/* <amenitiesToDisplay/> */}
+        <hr />
+      </div>
+    );
   }
 
   return (
-    <div> 
-     <div>{toShow}</div>
-     <div>{amenitiesToDisplay}</div>
+    <div>
+      <div>{toShow}</div>
+      <div>{amenitiesToDisplay}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Amenities
+export default Amenities;
